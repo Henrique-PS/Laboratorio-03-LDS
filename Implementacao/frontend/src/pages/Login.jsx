5df5f25db4e160ecb765d69e5681e6fc5cc9a7f9
Gabriel Pimentel Tabatinga <gpimenteltabatinga@gmail.com>
2023-10-16 10:18:09 -0300

    pagina login

--- Implementacao/frontend/src/pages/Login.jsx
+++ Implementacao/frontend/src/pages/Login.jsx
@@ -5,39 +5,70 @@ import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 
 import styled from 'styled-components';
-
+import "./login.css";
 
 const Wrapper = styled.div`
-    display: flex;
-    flex-direction: column;
-    min-height: 85vh;
-    justify-content: center;
-    align-items: center;
+
 `;
 
+// display: flex;
+// flex-direction: column;
+// min-height: 85vh;
+// justify-content: center;
+// align-items: center;
+
 const LoginForm = styled.form`
-    width: 300px; /* ou o tamanho desejado para o formulário */
-    margin: 20px;
+
 `;
+// width: 300px; /* ou o tamanho desejado para o formulário */
+// margin: 20px;
 
 const Login = () => {
     return (
         <div>
             <Navbar />
 
-            <Wrapper>
+            <div className="login">
+                <h4>Login</h4>
+                <form>
+                <div className="text_area">
+                    <input
+                    type="text"
+                    id="username"
+                    name="username"
+                    placeholder="email@examplo.com"
+                    className="text_input"
+
+                    />
+                </div>
+                <div className="text_area">
+                    <input
+                    type="password"
+                    id="password"
+                    name="password"
+                    placeholder="Senha"
+                    className="text_input"
+
+                    />
+                </div>
+                <button type="submit" className="btn">Login</button>
+                </form>
+                <a className="link" href="/signup">Sign Up</a>
+            </div>
+
+            {/* <Wrapper>
                 <LoginForm>
-                    <div className="mb-3">
-                        <label className="form-label">Email</label>
-                        <input type="email" className="form-control" defaultValue="email@example.com" />
-                    </div>
-                    <div className="mb-3">
-                        <label className="form-label">Password</label>
-                        <input type="password" className="form-control" placeholder="Password" />
-                    </div>
-                    <button type="submit" className="btn btn-primary">Login</button>
+                    <h4>Login</h4>
+                        <div className="mb-3 text_area">
+                            <input type="email" className="form-control text_input" placeholder="email@example.com" />
+                        </div>
+                        <div className="mb-3 text_area">
+                            <input type="password" className="form-control text_input" placeholder="Password" />
+                        </div>
+                        <button type="submit" className="btn btn-primary">Login</button>
                 </LoginForm>
-            </Wrapper>
+                <a className="link" href="/signup">Sign Up</a>
+            </Wrapper> */}
 
 
             <Footer />
